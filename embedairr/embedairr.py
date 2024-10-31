f9732a59734d7b3b9b69164271c408348743af8b
jahnz <friedsushi@protonmail.com>
2024-10-31 14:55:44 +0100

    Fix: cdr3 extraction is now independent from regular embeddings and attention matrix extraction method.

--- embedairr/embedairr.py
+++ embedairr/embedairr.py
@@ -20,7 +20,7 @@ def parse_arguments():
         "--output_path",
         type=str,
         required=True,
-        help="Directory for output files \n Will generate a subdirectory for outputs of each output_type.\nWill output multiple files if multiple layers are specified with '--layers'. Output file is a single tensor or a list of tensors when --pooling is False.",
+        help="Directory for output files \n Will generate a subdirectory for outputs of each output_type.\n Will output multiple files if multiple layers are specified with '--layers'. Output file is a single tensor or a list of tensors when --pooling is False.",
     )
     parser.add_argument(
         "--cdr3_path",
@@ -46,23 +46,29 @@ def parse_arguments():
         type=return_embeddings_type,
         default=True,
         nargs="?",
-        help="Set the embedding return type: None, True, False, or 'unpooled'.",
+        help="Set the embedding return type: True, False or 'unpooled'. Default is True.",
     )
     parser.add_argument(
         "--extract_cdr3_embeddings",
         type=return_embeddings_type,
         nargs="?",
         default=True,
-        help="Whether to pool the CDR3 extracted embeddings or not. Default is True.",
+        help="Set the embedding return type: True, False, or 'unpooled'. Requires --cdr3_path to be set. Default is True.",
     )
     parser.add_argument(
         "--extract_attention_matrices",
         type=return_attention_matrix_type,
         nargs="?",
         default="pooled",
+        help="Set the attention matrix return type: False, 'all_heads', 'average_layer' or 'average_all'. True returns = 'average_all'. Default is False.",
+    )
+    parser.add_argument(
+        "--extract_cdr3_attention_matrices",
+        type=return_attention_matrix_type,
+        nargs="?",
+        default="pooled",
         help="Whether to pool the CDR3 extracted embeddings or not. Default is True.",
     )
-    # TODO add argument return_attention_matrix
     # TODO add argument for batch_size
     # TODO add experiment name
     output_types = get_output_types(parser.parse_args())
@@ -136,17 +142,14 @@ def return_attention_matrix_type(value):
 # When changes made here, also update base_embedder.py BaseEmbedder.extract_batch() method.
 def get_output_types(args):
     output_types_dict = {
-        "embeddings": args.extract_embeddings is True and not bool(args.cdr3_path),
+        "embeddings": args.extract_embeddings is True,
         "embeddings_unpooled": args.extract_embeddings is "unpooled",
         "cdr3_extracted": args.extract_cdr3_embeddings is True and bool(args.cdr3_path),
         "cdr3_extracted_unpooled": args.extract_cdr3_embeddings is "unpooled",
-        "attention_matrices_average_all": args.extract_attention_matrices is "pooled"
-        and not bool(args.cdr3_path),
+        "attention_matrices_average_all": args.extract_attention_matrices is "pooled",
         "attention_matrices_average_layer": args.extract_attention_matrices
-        is "layer_pooled"
-        and not bool(args.cdr3_path),
-        "attention_matrices_all_heads": args.extract_attention_matrices is "unpooled"
-        and not bool(args.cdr3_path),
+        is "layer_pooled",
+        "attention_matrices_all_heads": args.extract_attention_matrices is "unpooled",
         "cdr3_attention_matrices_average_all": args.extract_attention_matrices
         is "pooled"
         and bool(args.cdr3_path),
