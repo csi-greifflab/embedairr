51fc5f6455791e22256ac23a2a73f8b1a587fcae
Jahn Zhong <jahnz@uio.no>
2025-07-09 19:58:22 +0200

    Fix: update model_name path to be relative

--- src/tests/test_run.sh
+++ src/tests/test_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pepe \
     --experiment_name "test" \
-    --model_name "/home/jahn/embedairr/examples/custom_model/example_protein_model" \
+    --model_name "examples/custom_model/example_protein_model" \
     --fasta_path "src/tests/test_files/test.fasta" \
     --output_path "src/tests/test_files/test_output" \
     --substring_path "src/tests/test_files/test_cdr3.csv" \
